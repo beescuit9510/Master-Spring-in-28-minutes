a46a5355b95bbff45319e2e275c24d992e09a36c
beescuit9510 <pengsoon20@gmail.com>
2022-04-05 09:50:55 +0900

    Constructor and setter injection

--- spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsApplication.java
+++ spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsApplication.java
@@ -11,14 +11,16 @@ public class SpringIn5StepsApplication {
     //To make best use of spring we need to tell 3 things to spring
     //What are the beans? //by adding @Component
     //What are the dependencies of a bean? //by adding @Autowired
-    //Where to search for beans?
-    // (by adding @ComponentScan or @SpringBootApplication)
-    // (it automatically scans the package and the sub-packages of the package where this is in)
-    // => no need (bcz BinarySearchImpl and BubbleSortAlgorithm is in the same package)
+    //Where to search for beans? // by adding @ComponentScan or @SpringBootApplication (it automatically scans the package and the sub-packages of the package where this is in)
+    // for now => no need (bcz BinarySearchImpl and BubbleSortAlgorithm is in the same package)
 
     // ApplicationContext where all the beans going to be present and managed
+    //Spring creates, injects and manages dependencies, manages the entire lifecycle of all the beans
 
-    //Spring creates , injects and manages dependencies, manages the entire lifecycle of all the beans
+    //whether using setter or field both are same in the way spring dose the autowiring is the same
+    //in the earlier days mandatory dependency should have been autowired using constructor and optional dependency went for setter injection
+    // but nowadays there is no big difference between mandatory and optional dependencies
+    // ( since Spring dose not log setter and field injection it is recommended to use constructor)
 
     public static void main(String[] args) {
 
@@ -26,7 +28,7 @@ public class SpringIn5StepsApplication {
 
         BinarySearchImp binarySearch = applicationContext.getBean(BinarySearchImp.class);
 
-        int result = binarySearch.binarySearch(new int[]{1,2,3,4},1);
+        int result = binarySearch.binarySearch(new int[]{1, 2, 3, 4}, 1);
 
         System.out.println(result);
     }
